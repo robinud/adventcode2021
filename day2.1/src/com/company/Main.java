77ff8721695cf7cd345d3c8d9562a40647fbaf2b
Robin <38460664+robinud@users.noreply.github.com>
2021-12-02 09:44:53 +0100

    Puzzle day 2.1

---
+++ day2.1/src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            File input = new File("input.txt");
+            Scanner myReader = new Scanner(input);
+            int depth = 0;
+            int horizontal = 0;
+
+            while (myReader.hasNextLine()) {
+                String line = myReader.nextLine();
+                int numberOnly = Integer.parseInt(line.replaceAll("[^0-9]", ""));
+                if(line.contains("forward")){
+                    horizontal += numberOnly;
+                }
+                else if(line.contains("up")){
+                    depth -= numberOnly;
+                }
+                else if(line.contains("down")){
+                    depth += numberOnly;
+                }
+            }
+            int finalResult = depth * horizontal;
+            System.out.println("The end result is:" + finalResult);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
