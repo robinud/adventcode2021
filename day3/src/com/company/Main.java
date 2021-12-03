6a0d5e15e29fba543ef33dbcafa6d5090097b953
Robin <38460664+robinud@users.noreply.github.com>
2021-12-03 09:45:05 +0100

    day 3 puzzle 2

--- day3/src/com/company/Main.java
+++ day3/src/com/company/Main.java
@@ -2,12 +2,12 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    
 
-    public static void main(String[] args) {
+    public void puzzle1(){
         try {
             File input = new File("input.txt");
             Scanner myReader = new Scanner(input);
@@ -45,4 +45,79 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    public static ArrayList<String> removeBinaries(ArrayList<String> list, int binaryCode, int index){
+        if(list.size() == 1){
+            return list;
+        }
+        ArrayList<String> newValues = new ArrayList<>();
+        for(String code: list){
+            if(!(code.charAt(index) == Character.forDigit(binaryCode, 10))){
+                newValues.add(code);
+            }
+        }
+        return newValues;
+    }
+
+    public static void main(String[] args) {
+        try {
+            File input = new File("input.txt");
+            Scanner myReader = new Scanner(input);
+            ArrayList<String> binaryCodes = new ArrayList();
+            int[] zeroCounters = new int[12];
+            int[] oneCounters = new int[12];
+            while (myReader.hasNextLine()) {
+                String binaryCode = myReader.nextLine();
+                binaryCodes.add(binaryCode);
+                for(int i=0; i < binaryCode.length(); i++){
+                    if(binaryCode.charAt(i) == '0'){
+                        zeroCounters[i] += 1;
+                    }
+                    else if(binaryCode.charAt(i) == '1'){
+                        oneCounters[i] += 1;
+                    }
+                }
+            }
+            ArrayList<String> oxygen = binaryCodes;
+            ArrayList<String> co2 = binaryCodes;
+            for(int i = 0; i< zeroCounters.length; i++){
+                boolean hasMoreZeroesOxygen = countBinary(oxygen, i);
+                boolean hasMoreZeroesCo2 = countBinary(co2, i);
+                if(hasMoreZeroesOxygen){
+                    oxygen = removeBinaries(oxygen, 1, i);
+                }
+                else{
+                    oxygen = removeBinaries(oxygen, 0, i);
+                }
+                if(hasMoreZeroesCo2){
+                    co2 = removeBinaries(co2, 0, i);
+                }
+                else {
+                    co2 = removeBinaries(co2, 1, i);
+                }
+            }
+            int finalOxygen = Integer.parseInt(oxygen.get(0),2 );
+            int finalCo2 = Integer.parseInt(co2.get(0), 2);
+            System.out.println("The end result is:" + finalOxygen * finalCo2);
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    private static boolean countBinary(ArrayList<String> list, int index) {
+        int zeroCounter = 0;
+        int oneCounter = 0;
+        for(String code: list){
+            if((code.charAt(index) == Character.forDigit(0, 10))){
+                zeroCounter++;
+            }
+            else{
+                oneCounter++;
+            }
+        }
+        return zeroCounter > oneCounter;
+    }
+
 }
